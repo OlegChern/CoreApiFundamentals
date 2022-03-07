a693ff610fe0189d79597a9b6c838bdfc480c628
OlegChern <OLChernyavsky@mail.ru>
2022-03-07 23:47:18 +0300

    Finished 'Building Your First API' module

---
+++ src/Data/CampProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using CoreCodeCamp.Models;
+
+namespace CoreCodeCamp.Data
+{
+    public class CampProfile : Profile
+    {
+        public CampProfile()
+        {
+            CreateMap<Talk, TalkModel>();
+            CreateMap<Speaker, SpeakerModel>();
+            CreateMap<Camp, CampModel>()
+                .ForMember(m => m.Venue, o => o.MapFrom(m => m.Location.VenueName));
+        }
+    }
+}
