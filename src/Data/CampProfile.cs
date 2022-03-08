3fdf8b5f64b083496fcc26e139c3f667dfe5a1b3
OlegChern <OLChernyavsky@mail.ru>
2022-03-08 13:17:26 +0300

    Finished the 'Creating Association APIs' module

--- src/Data/CampProfile.cs
+++ src/Data/CampProfile.cs
@@ -7,8 +7,14 @@ namespace CoreCodeCamp.Data
     {
         public CampProfile()
         {
-            CreateMap<Talk, TalkModel>();
-            CreateMap<Speaker, SpeakerModel>();
+            CreateMap<Talk, TalkModel>()
+                .ReverseMap()
+                .ForMember(t => t.Camp, opt => opt.Ignore())
+                .ForMember(t => t.Speaker, opt => opt.Ignore());
+
+            CreateMap<Speaker, SpeakerModel>()
+                .ReverseMap();
+
             CreateMap<Camp, CampModel>()
                 .ForMember(m => m.Venue, o => o.MapFrom(m => m.Location.VenueName))
                 .ReverseMap();
